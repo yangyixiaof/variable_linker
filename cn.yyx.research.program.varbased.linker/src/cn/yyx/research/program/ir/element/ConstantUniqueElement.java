ca49e2349d76abfce8f5d37be34001a3167d0f5c
DELL <DELL@DESKTOP-20DEPKI>
2017-07-14 12:13:25 +0800

    small change.

--- cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/ir/element/ConstantUniqueElement.java
+++ cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/ir/element/ConstantUniqueElement.java
@@ -1,15 +1,5 @@
 package cn.yyx.research.program.ir.element;
 
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.jobs.ISchedulingRule;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IJavaModel;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IOpenable;
-import org.eclipse.jdt.core.JavaModelException;
-
 import cn.yyx.research.program.ir.IRElementMeta;
 
 public class ConstantUniqueElement extends VirtualDefinedElement {
@@ -17,118 +7,17 @@ public class ConstantUniqueElement extends VirtualDefinedElement {
 	private String represent = null;
 	
 	public ConstantUniqueElement(String represent) {
-		this.represent = represent;
+		super(represent);
 	}
 	
-	@Override
-	public <T> T getAdapter(Class<T> adapter) {
-		return null;
-	}
-
-	@Override
-	public boolean exists() {
-		return false;
-	}
-
-	@Override
-	public IJavaElement getAncestor(int ancestorType) {
-		return null;
-	}
-
-	@Override
-	public String getAttachedJavadoc(IProgressMonitor monitor) throws JavaModelException {
-		return null;
-	}
-
-	@Override
-	public IResource getCorrespondingResource() throws JavaModelException {
-		return null;
-	}
-
 	@Override
 	public String getElementName() {
 		return IRElementMeta.ConstantUniqueElement + "#" + represent;
 	}
-
-	@Override
-	public int getElementType() {
-		return 0;
-	}
-
-	@Override
-	public String getHandleIdentifier() {
-		return represent;
-	}
-
-	@Override
-	public IJavaModel getJavaModel() {
-		return null;
-	}
-
-	@Override
-	public IJavaProject getJavaProject() {
-		return null;
-	}
-
-	@Override
-	public IOpenable getOpenable() {
-		return null;
-	}
-
-	@Override
-	public IJavaElement getParent() {
-		return null;
-	}
-
-	@Override
-	public IPath getPath() {
-		return null;
-	}
-
-	@Override
-	public IJavaElement getPrimaryElement() {
-		return null;
-	}
-
-	@Override
-	public IResource getResource() {
-		return null;
-	}
-
-	@Override
-	public ISchedulingRule getSchedulingRule() {
-		return null;
-	}
-
-	@Override
-	public IResource getUnderlyingResource() throws JavaModelException {
-		return null;
-	}
-
-	@Override
-	public boolean isReadOnly() {
-		return false;
-	}
-
-	@Override
-	public boolean isStructureKnown() throws JavaModelException {
-		return false;
-	}
-
-	@Override
-	public String toString() {
-		return getElementName();
-	}
 	
 	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof ConstantUniqueElement) {
-			ConstantUniqueElement cue = (ConstantUniqueElement)obj;
-			if (represent.equals(cue.represent)) {
-				return true;
-			}
-		}
-		return super.equals(obj);
+	public String getHandleIdentifier() {
+		return IRElementMeta.ConstantUniqueElement + "#" + represent;
 	}
 	
 }
--- cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/ir/element/UnresolvedNameOrFieldAccessElement.java
+++ cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/ir/element/UnresolvedNameOrFieldAccessElement.java
@@ -1,123 +1,21 @@
 package cn.yyx.research.program.ir.element;
 
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.jobs.ISchedulingRule;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IJavaModel;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IOpenable;
-import org.eclipse.jdt.core.JavaModelException;
-
 import cn.yyx.research.program.ir.IRElementMeta;
 
 public class UnresolvedNameOrFieldAccessElement extends VirtualDefinedElement {
 	
-	String reference = null;
-	
 	public UnresolvedNameOrFieldAccessElement(String reference) {
-		this.reference = reference;
+		super(reference);
 	}
 	
-	@Override
-	public <T> T getAdapter(Class<T> adapter) {
-		return null;
-	}
-
-	@Override
-	public boolean exists() {
-		return false;
-	}
-
-	@Override
-	public IJavaElement getAncestor(int ancestorType) {
-		return null;
-	}
-
-	@Override
-	public String getAttachedJavadoc(IProgressMonitor monitor) throws JavaModelException {
-		return null;
-	}
-
-	@Override
-	public IResource getCorrespondingResource() throws JavaModelException {
-		return null;
-	}
-
 	@Override
 	public String getElementName() {
-		return IRElementMeta.UnresolvedNameOrFieldAccessElement + "#" + reference;
+		return IRElementMeta.UnresolvedNameOrFieldAccessElement + "#" + represent;
 	}
-
-	@Override
-	public int getElementType() {
-		return 0;
-	}
-
+	
 	@Override
 	public String getHandleIdentifier() {
-		return null;
-	}
-
-	@Override
-	public IJavaModel getJavaModel() {
-		return null;
-	}
-
-	@Override
-	public IJavaProject getJavaProject() {
-		return null;
-	}
-
-	@Override
-	public IOpenable getOpenable() {
-		return null;
-	}
-
-	@Override
-	public IJavaElement getParent() {
-		return null;
-	}
-
-	@Override
-	public IPath getPath() {
-		return null;
-	}
-
-	@Override
-	public IJavaElement getPrimaryElement() {
-		return null;
-	}
-
-	@Override
-	public IResource getResource() {
-		return null;
-	}
-
-	@Override
-	public ISchedulingRule getSchedulingRule() {
-		return null;
-	}
-
-	@Override
-	public IResource getUnderlyingResource() throws JavaModelException {
-		return null;
-	}
-
-	@Override
-	public boolean isReadOnly() {
-		return false;
-	}
-
-	@Override
-	public boolean isStructureKnown() throws JavaModelException {
-		return false;
-	}
-
-	@Override
-	public String toString() {
-		return getElementName();
+		return IRElementMeta.UnresolvedNameOrFieldAccessElement + "#" + represent;
 	}
 	
 }
