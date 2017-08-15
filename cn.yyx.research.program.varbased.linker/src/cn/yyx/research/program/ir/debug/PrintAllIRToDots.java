476709c927cdc050d2c9e5b00d8c8c73eae938ad
DELL <DELL@DESKTOP-20DEPKI>
2017-08-15 16:26:30 +0800

    small change.

---
+++ cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/ir/debug/PrintAllIRToDots.java
@@ -0,0 +1,33 @@
+package cn.yyx.research.program.ir.debug;
+
+import cn.yyx.research.program.ir.storage.IRGraphManager;
+
+public class PrintAllIRToDots {
+	
+	public PrintAllIRToDots(IRGraphManager irgfop)
+	{
+//		Set<IType> types = irgfop.GetAllClasses();
+//		Iterator<IType> citr = types.iterator();
+//		while (citr.hasNext())
+//		{
+//			IType it = citr.next();
+//			IRForOneClass irfoc = irgfop.GetClassIR(it);
+//			// debugging.
+//			System.out.println(irfoc);
+//			
+//			StringBuffer dot_content = new StringBuffer("");
+//			dot_content.append("digraph {\n");
+//			dot_content.append("edge[fontname=\"SimSun\",fontcolor=red];\n");
+//			dot_content.append("node[fontname=\"SimSun\",size=\"20,20\"];\n");
+//			dot_content.append("\n");
+//			
+//			dot_content.append("21->22;\n");
+//			
+//			
+//			dot_content.append("\n}\n");
+//			
+//			FileUtil.WriteToFile(new File(IRVisualMeta.dot_directory + "/" + it.getFullyQualifiedName()), dot_content.toString());
+//		}
+	}
+	
+}
