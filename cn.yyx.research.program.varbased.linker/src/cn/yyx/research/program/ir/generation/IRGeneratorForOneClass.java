ffabb4b0af38f230ffafaa2cfab4b8049e3ef5a6
DELL <DELL@DESKTOP-20DEPKI>
2017-08-15 16:14:39 +0800

    small change.

--- cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/ir/generation/IRGeneratorForOneClass.java
+++ cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/ir/generation/IRGeneratorForOneClass.java
@@ -8,7 +8,6 @@ import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
-import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
 import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.IMethodBinding;
@@ -27,18 +26,18 @@ public class IRGeneratorForOneClass extends IRGeneratorForStatements {
 
 	private Initializer initial_node = null;
 	private IType it = null;
-	
+
 	public IRGeneratorForOneClass(IType it, IJavaProject java_project, IRGraph graph, IRGraphManager graph_manager,
 			IRElementPool pool) {
 		super(java_project, null, graph, graph_manager, pool, null, it);
 		this.it = it;
 	}
-	
-//	public IRGeneratorForOneClass(IType it) {
-//		super(null, new IRForOneField(it));
-//		this.it = it;
-//	}
-		
+
+	// public IRGeneratorForOneClass(IType it) {
+	// super(null, new IRForOneField(it));
+	// this.it = it;
+	// }
+
 	@Override
 	public boolean visit(Initializer node) {
 		this.initial_node = node;
@@ -50,6 +49,7 @@ public class IRGeneratorForOneClass extends IRGeneratorForStatements {
 		// no need to do anything.
 		return super.visit(node);
 	}
+
 	// Solved. return element needs to be handled.
 	@SuppressWarnings("unchecked")
 	@Override
@@ -57,44 +57,54 @@ public class IRGeneratorForOneClass extends IRGeneratorForStatements {
 		IMethodBinding imb = node.resolveBinding();
 		IJavaElement ije = imb.getJavaElement();
 		if (ije instanceof IMethod) {
-			IMethod im = (IMethod)ije;
+			IMethod im = (IMethod) ije;
 			List<SingleVariableDeclaration> para_list = node.parameters();
-			IRGeneratorHelper.HandleMethodDeclaration(java_project, graph_manager, node.getBody(), pool, imb, im, it, para_list, super_class_element);
+			IRGeneratorHelper.HandleMethodDeclaration(java_project, graph_manager, node.getBody(), pool, imb, im, it,
+					para_list, super_class_element);
 		}
 		return false;
 	}
-	
+
 	@Override
-	public void preVisit(ASTNode node) {
-		super.preVisit(node);
-		if (node instanceof AbstractTypeDeclaration || node instanceof AnonymousClassDeclaration)
-		{
+	public boolean preVisit2(ASTNode node) {
+		if (node instanceof AbstractTypeDeclaration || node instanceof AnonymousClassDeclaration) {
 			IType resolved_type = NodeBinding(node);
-			boolean type_equals = resolved_type == null ? false : resolved_type.equals(it);
-			// boolean has_element = irc.IsHasElement();
-			if (type_equals) {// && has_element
-				if (node instanceof TypeDeclaration || node instanceof AnnotationTypeDeclaration) {
-					Type super_type = ((TypeDeclaration)node).getSuperclassType();
+			if (resolved_type != null) {
+				boolean type_equals = resolved_type.equals(it);// resolved_type == null ? false : 
+				IType super_it = null;
+				// boolean has_element = irc.IsHasElement();
+				if (node instanceof TypeDeclaration) {
+					Type super_type = ((TypeDeclaration) node).getSuperclassType();
 					ITypeBinding itb = super_type.resolveBinding();
 					if (itb != null) {
 						IJavaElement super_ije = itb.getJavaElement();
 						if (super_ije != null) {
-							IType super_it = (IType)super_ije;
-							this.super_class_element = pool.UniversalElement(super_it.getElementName(), super_it);
+							super_it = (IType) super_ije;
 						}
 					}
 				}
-				// IRGeneratorForOneProject.GetInstance().FetchITypeIR((it)).SetFieldLevel((IRForOneField)irc);
+				if (type_equals) {// && has_element
+					this.super_class_element = pool.UniversalElement(super_it.getElementName(), super_it);
+					// IRGeneratorForOneProject.GetInstance().FetchITypeIR((it)).SetFieldLevel((IRForOneField)irc);
+				} else {
+					IRGraph graph = new IRGraph();
+					graph_manager.AddIRGraph(resolved_type, graph);
+					IRGeneratorForOneClass irgfoc = new IRGeneratorForOneClass(resolved_type, java_project, graph,
+							graph_manager, pool);
+					node.accept(irgfoc);
+				}
+			} else {
+				return super.preVisit2(node) && false;
 			}
 		}
+		return super.preVisit2(node);
 	}
-	
-	// TODO type should be visited.
-	
+
+	// Solved. internal type should be visited.
+
 	@Override
 	public void postVisit(ASTNode node) {
-		if (node instanceof AbstractTypeDeclaration || node instanceof AnonymousClassDeclaration)
-		{
+		if (node instanceof AbstractTypeDeclaration || node instanceof AnonymousClassDeclaration) {
 			IType resolved_type = NodeBinding(node);
 			boolean type_equals = resolved_type == null ? false : resolved_type.equals(it);
 			// boolean has_element = irc.IsHasElement();
@@ -107,25 +117,25 @@ public class IRGeneratorForOneClass extends IRGeneratorForStatements {
 		}
 		super.postVisit(node);
 	}
-	
+
 	private IType NodeBinding(ASTNode node) {
 		if (node instanceof AbstractTypeDeclaration) {
-			AbstractTypeDeclaration atd = (AbstractTypeDeclaration)node;
+			AbstractTypeDeclaration atd = (AbstractTypeDeclaration) node;
 			ITypeBinding tb = atd.resolveBinding();
 			if (tb != null) {
 				IJavaElement tele = tb.getJavaElement();
 				if (tele != null && tele instanceof IType) {
-					return (IType)tele;
+					return (IType) tele;
 				}
 			}
 		}
 		if (node instanceof AnonymousClassDeclaration) {
-			AnonymousClassDeclaration atd = (AnonymousClassDeclaration)node;
+			AnonymousClassDeclaration atd = (AnonymousClassDeclaration) node;
 			ITypeBinding tb = atd.resolveBinding();
 			if (tb != null) {
 				IJavaElement tele = tb.getJavaElement();
 				if (tele != null && tele instanceof IType) {
-					return (IType)tele;
+					return (IType) tele;
 				}
 			}
 		}
