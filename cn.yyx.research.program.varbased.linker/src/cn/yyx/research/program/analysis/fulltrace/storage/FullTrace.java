7c43a79f3231d9f0133532d01276c3e14a0593e5
DELL <DELL@DESKTOP-20DEPKI>
2017-08-15 23:05:01 +0800

    small change.

--- cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/analysis/fulltrace/storage/FullTrace.java
+++ cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/analysis/fulltrace/storage/FullTrace.java
@@ -1,22 +1,9 @@
 package cn.yyx.research.program.analysis.fulltrace.storage;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
 
-import org.eclipse.jdt.core.IJavaElement;
-
-import cn.yyx.research.program.analysis.fulltrace.storage.connection.DynamicConnection;
-import cn.yyx.research.program.analysis.fulltrace.storage.node.DynamicNode;
-import cn.yyx.research.program.ir.meta.IRMeta;
 import cn.yyx.research.program.ir.storage.IRGraph;
-import cn.yyx.research.program.ir.storage.connection.ConnectionInfo;
-import cn.yyx.research.program.ir.storage.connection.EdgeBaseType;
-import cn.yyx.research.program.ir.storage.connection.EdgeTypeUtil;
-import cn.yyx.research.program.ir.storage.node.lowlevel.IRForOneBranchControl;
-import cn.yyx.research.program.ir.storage.node.lowlevel.IRForOneInstruction;
 import cn.yyx.research.program.ir.visual.node.IVNode;
 import cn.yyx.research.program.ir.visual.node.connection.IVConnection;
 import cn.yyx.research.program.ir.visual.node.container.IVNodeContainer;
