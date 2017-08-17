2f82fa0d855bd9ba74a43b50792f07fabee594f0
DELL <DELL@DESKTOP-20DEPKI>
2017-08-17 10:41:13 +0800

    small change.

--- cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/analysis/fulltrace/FullTraceMeta.java
+++
@@ -1,5 +0,0 @@
-package cn.yyx.research.program.analysis.fulltrace;
-
-public class FullTraceMeta {
-
-}
---
+++ cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/analysis/fulltrace/meta/FullTraceMeta.java
@@ -0,0 +1,5 @@
+package cn.yyx.research.program.analysis.fulltrace.meta;
+
+public class FullTraceMeta {
+
+}
