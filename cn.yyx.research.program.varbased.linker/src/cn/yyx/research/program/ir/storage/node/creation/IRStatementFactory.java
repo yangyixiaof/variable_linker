61bc84cadee871639fc0c3445b878c41c2953239
DELL <DELL@DESKTOP-20DEPKI>
2017-08-17 11:53:49 +0800

    small change.

--- cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/ir/storage/node/creation/IRStatementFactory.java
+++ cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/ir/storage/node/creation/IRStatementFactory.java
@@ -1,16 +1,22 @@
 package cn.yyx.research.program.ir.storage.node.creation;
 
-import java.util.TreeMap;
+import java.util.LinkedList;
+import java.util.List;
 
 import cn.yyx.research.program.ir.storage.node.IRStatementNode;
 
 public class IRStatementFactory {
 	
-	TreeMap<String, IRStatementNode> statements = new TreeMap<String, IRStatementNode>();
+	List<IRStatementNode> statements = new LinkedList<IRStatementNode>();
 	
 	public IRStatementFactory() {
 	}
 	
-	
+	public IRStatementNode UniversalStatement(IRStatementNode irsn) {
+		// IRJavaElement irje
+		// String content = irje.getElement().toString();
+		statements.add(irsn);
+		return irsn;
+	}
 	
 }
