054d7f4eb988bc23fcd8a9d2e81738c479a1a07c
DELL <DELL@DESKTOP-20DEPKI>
2017-08-29 11:32:54 +0800

    small change.

--- cn.yyx.research.program.varbased.linker/src/cn/yyx/research/todos/TodoList.java
+++ cn.yyx.research.program.varbased.linker/src/cn/yyx/research/todos/TodoList.java
@@ -5,7 +5,7 @@ public class TodoList {
 	// Suppressed. the following problem is some what solved due to extracting features on graphs.
 	// How to recognize relationships including code snippet not on the execution path from main root?
 	
-	// TODO add connection from statements to souce_resolved_type/souce_resolved_method to help select.
+	// Solved. add connection from statements to souce_resolved_type/souce_resolved_method to help select.
 	// Ignored. how to copy return nodes and connect separate node to its input.
 	
 	// Solved. Bug: variables are taken as names. This needs to be fixed.
