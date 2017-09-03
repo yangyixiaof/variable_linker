c3b9b46645985add35e87b2b91e1a2ed0188354d
DELL <DELL@DESKTOP-20DEPKI>
2017-09-03 14:34:28 +0800

    small change.

--- cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/analysis/prepare/PreProcessHelper.java
+++ cn.yyx.research.program.varbased.linker/src/cn/yyx/research/program/analysis/prepare/PreProcessHelper.java
@@ -2,9 +2,14 @@ package cn.yyx.research.program.analysis.prepare;
 
 import java.util.List;
 
+import org.eclipse.jdt.core.IBuffer;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.ToolFactory;
+import org.eclipse.jdt.core.formatter.CodeFormatter;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.text.edits.TextEdit;
 
 import cn.yyx.research.program.eclipse.searchutil.EclipseSearchForICompilationUnits;
@@ -17,30 +22,28 @@ public class PreProcessHelper {
 		// System.err.println("unit_size:" + units.size());
 		for (final ICompilationUnit compilation_resource : units) {
 			try {
-				TextEdit edit1 = PreProcessCompilationUnitHelper.PreProcessTransformer(compilation_resource, java_project);
-				compilation_resource.applyTextEdit(edit1, null);
+				TextEdit edit = PreProcessCompilationUnitHelper.PreProcessTransformer(compilation_resource, java_project);
+				compilation_resource.applyTextEdit(edit, null);
 				compilation_resource.reconcile(ICompilationUnit.NO_AST, false, compilation_resource.getOwner(), null);
 				compilation_resource.save(null, false);
 
-//				String changed_class = PreProcessCompilationUnitHelper.PreProcessDeleter(compilation_resource,
-//						java_project);
-//				CodeFormatter codeFormatter = ToolFactory.createCodeFormatter(null);
-//				TextEdit textEdit = codeFormatter.format(CodeFormatter.K_COMPILATION_UNIT, changed_class, 0,
-//						changed_class.length(), 0, null);
-//				IDocument doc = new Document(changed_class);
-//				try {
-//					textEdit.apply(doc);
-//					changed_class = doc.get();
-//				} catch (MalformedTreeException e) {
-//					e.printStackTrace();
-//				} catch (BadLocationException e) {
-//					e.printStackTrace();
-//				}
-//
-//				IBuffer ibuf = compilation_resource.getBuffer();
-//				ibuf.setContents(changed_class);
-//				compilation_resource.reconcile(ICompilationUnit.NO_AST, false, compilation_resource.getOwner(), null);
-//				compilation_resource.save(null, false);
+				String changed_class = PreProcessCompilationUnitHelper.PreProcessDeleter(compilation_resource,
+						java_project);
+				CodeFormatter codeFormatter = ToolFactory.createCodeFormatter(null);
+				TextEdit textEdit = codeFormatter.format(CodeFormatter.K_COMPILATION_UNIT, changed_class, 0,
+						changed_class.length(), 0, null);
+				IDocument doc = new Document(changed_class);
+				try {
+					textEdit.apply(doc);
+					changed_class = doc.get();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				IBuffer ibuf = compilation_resource.getBuffer();
+				ibuf.setContents(changed_class);
+				compilation_resource.reconcile(ICompilationUnit.NO_AST, false, compilation_resource.getOwner(), null);
+				compilation_resource.save(null, false);
 			} catch (Exception e) {
 			}
 		}
